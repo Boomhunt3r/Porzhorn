0a2ccec13934ea7e42e658ab689e1b7d214e5b01
Boomhunter <jannekvieth@outlook.de>
2019-02-06 10:41:54 +0100

    Alter Npc aufruf im Player entfernt, ggf checken ob er bei euch wieder da ist. Animation Klasse wird befüllt

--- SDL2_Engine/Player.cpp
+++ SDL2_Engine/Player.cpp
@@ -236,15 +236,6 @@ void GPlayer::Update(float _deltaSeconds)
 	// if camera x and y not in range
 	else
 		RENDERER->SetCamera(SVector2(RENDERER->GetCamera().X, RENDERER->GetCamera().Y));
-
-	// if player hits NPC
-	if (m_position.X >= 256 - m_rect.w && m_position.X <= 350 + 32 && m_position.Y <= 550 + 54 && m_position.Y >= 468 - m_rect.h)
-	{
-		// create text and add to ctm
-		CText* pNPCText = new CText("Hier kommt eine Story hin !", GAME->m_PGaramond, SRect(SVector2(300, 400), SVector2(500, 50)), SColor(255, 255, 255));
-		pNPCText->SetInWorld(true);
-		CTM->AddUIObject(pNPCText);
-	}
 }
 
 // render every frame
